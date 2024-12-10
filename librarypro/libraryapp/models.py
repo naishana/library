12ed74e7463ebaffcae3dc8c0e516cc468cc5f88
Naishana <naishanasaiphu@gmail.com>
2024-12-10 11:20:30 +0530

    initial commit

---
+++ librarypro/libraryapp/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+class book(models.Model):
+    Name=models.CharField(max_length=100)
+    Author=models.CharField(max_length=100)
+    price=models.IntegerField()
+    Image=models.ImageField(upload_to='images/')
+    is_featured=models.BooleanField(default=False)
+    is_popular=models.BooleanField(default=False)
+    is_offer=models.BooleanField(default=False)
+    def _str_(self):
+        return self.Name
+
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(book, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def _str_(self):
+        return f"{self.user.username} - {self.product.name} - {self.quantity}"
+
+    @property
+    def total_price(self):
+        return self.product.price * self.quantity
+# class popular(models.Model):
+#     Name= models.CharField(max_length=200)
+#     Author= models.CharField(max_length=200)
+#     Price= models.FloatField()
+#     Image= models.ImageField(upload_to='images/')
+
+    # def __str__(self):
+    #     return self.Name                      
+
